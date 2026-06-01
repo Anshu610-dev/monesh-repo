d85179229638902566a2166e009057486954336e
Anshu Pal <anshulpal25@iitk.ac.in>
2026-06-02 00:00:33 +0530

    Delete life-Tracker-OS-main-3phase--main (5)/life-Tracker-OS-main-3phase--main directory

--- life-Tracker-OS-main-3phase--main (5)/life-Tracker-OS-main-3phase--main/src/context/appContextCore.js
+++
@@ -1,9 +0,0 @@
-import { createContext, useContext } from 'react'
-
-export const AuthContext = createContext(null)
-export const AppStateContext = createContext(null)
-export const AppActionsContext = createContext(null)
-
-export function useAuth() {
-  return useContext(AuthContext)
-}
